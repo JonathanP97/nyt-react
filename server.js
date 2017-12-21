90366fc466566974a1871d31e1252ed3d8fd3d29
Jonathan Plata <jonathan_plata@ymail.com>
2017-12-21 12:03:11 -0800

    minor changes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-// var cheerio = require('cheerio');
+var cheerio = require('cheerio');
 var request = require('request');
-// var axios = require('axios');
+var axios = require('axios');
 
 var PORT = 3000;
 var app = express();
@@ -15,8 +15,29 @@ mongoose.connect("mongodb://localhost/nytreact", {
 
 app.listen(PORT, () => {
 	console.log("Running on port: " + PORT);
+	getArticle();
 })
 
-app.get("/home", (req,res) => {
-	res.send("hi");
+app.get("/", (req,res) => {
+	console.log("hi");
 });
+
+app.get("/api/articles", (req,res) => {
+	// res.json()
+});
+
+
+function getArticle() {
+	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+	url += "?api-key=cecdb7f428d1453f9c06b3d679b784eb&q=california" 
+	
+	$.ajax({
+		url: url,
+		method: "GET",
+	}).done(res => {
+		console.log(res);
+	}).fail(err => {
+		throw err;	
+	});
+}
+
