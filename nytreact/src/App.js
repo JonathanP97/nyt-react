3831c3a90a1ac0c91df4de8d3e475125fd9f46a1
Jonathan Plata <jonathan_plata@ymail.com>
2017-12-21 21:00:40 -0800

    Home component ajax

--- nytreact/src/App.js
+++ nytreact/src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import Home from "./components/Home";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">New York Time React</h1>
-        </header>
-        <p className="App-intro">
-          save to reload
-        </p>
+      <div>
+        <Home />
       </div>
     );
   }
---
+++ nytreact/src/components/Home/Home.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+class Home extends Component {
+  constructor(props) {
+  	super(props);
+
+    this.state = {
+      topic: "",
+      start: "",
+      end: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const name = event.target.name;
+
+    this.setState({
+      [name]: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.topic);
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=cecdb7f428d1453f9c06b3d679b784eb";
+    url += "q=" + this.state.topic;
+    // if(this.state.start) {
+    //   url+= 
+    // } 
+    fetch(url).then(result => {
+      console.log(result);
+    });
+
+  }
+
+  render() {
+  	return(
+      <div>
+        <div className="header">
+          <h2>Nyt React</h2>
+        </div>
+        <div className="search">
+            <h4>Search</h4>
+            <div>
+              <form onSubmit={this.handleSubmit}>
+                <label>Topic</label>
+                <input 
+                  type="text"
+                  name="topic"
+                  onChange={this.handleChange}
+                />
+                <label>Start Year</label>
+                <input 
+                  type="text"
+                  name="start"
+                  onChange={this.handleChange}
+                />
+                <label>End Year</label>
+                <input 
+                  type="text"
+                  name="end"
+                  onChange={this.handleChange}
+                />
+                <button>Search</button>
+              </form>
+            </div>
+        </div>
+      </div>
+  	);
+  }
+}
+
+export default Home;
