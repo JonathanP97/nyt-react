ea510b1eb6bf907724b7e2b479083c3507d45796
Jonathan Plata <jonathan_plata@ymail.com>
2018-01-18 18:14:46 -0800

    deploy

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var axios = require('axios');
 var morgan = require('morgan');
 var db = require('./models');
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 var app = express();
 
 mongoose.Promise = Promise;
